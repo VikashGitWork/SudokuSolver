5c10712e3279f23e061c2eff859f3a086552bd1d
VikashGitWork <vikash.choudhari07@gmail.com>
2017-04-05 00:47:53 +0200

    Addition Of Test Project & Tests

---
+++ SudokuSolverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SudokuSolverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Infor")]
+[assembly: AssemblyProduct("SudokuSolverTest")]
+[assembly: AssemblyCopyright("Copyright © Infor 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3eb4741f-366d-4d81-9cff-4c4f82615473")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
